cc9aab354c8938d2cb05deaf1bb5a9cde794b265
Byungkook Jang <bkoook@gmail.com>
2014-04-20 20:54:33 +0900

    Initial Commit

---
+++ BKPasscodeViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BKPasscodeViewDemo
+//
+//  Created by Byungkook Jang on 2014. 4. 20..
+//  Copyright (c) 2014년 Byungkook Jang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BKPasscodeViewDemo/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BKPasscodeViewDemo
+//
+//  Created by Byungkook Jang on 2014. 4. 20..
+//  Copyright (c) 2014년 Byungkook Jang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
